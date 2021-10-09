beaa3429ff3b2866c51b0dc67edca384e10cf27b
Fernanda de Camargo <fernanda.magano@alumni.usp.br>
2021-10-09 14:52:16 -0300

    [k8s-ns] YAML file to create service in k8s

--- create-k8s-resources.sh
+++ create-k8s-resources.sh
@@ -3,3 +3,9 @@ kubectl apply -f namespace.yaml
 echo "******* Existing namespaces ********"
 kubectl get ns
 echo "************************************"
+
+kubectl apply -f svc.yaml
+echo "******* Existing services ********"
+kubectl -n python-test get svc
+echo "**********************************"
+
